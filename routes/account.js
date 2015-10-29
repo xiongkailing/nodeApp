01c4662a1f4d19ba609cdd6d7708be0dfec3b3ee
kailing_bear <349419669@qq.com>
2015-10-29 21:58:17 +0800

    将swig设置为视图引擎 完成项目主题结构

---
+++ routes/account.js
@@ -0,0 +1,24 @@
+var express=require('express');
+var router=express.Router();
+
+router.get('/login', function (req,res,next) {
+
+});
+router.post('/login',function(req,res,next){
+
+});
+router.get('/register',function(req,res,next){
+
+});
+
+router.post('/register',function(req,res,next){
+
+});
+
+router.get('/pwd',function(req,res,next){
+
+});
+
+router.get('/pwdcallback',function(req,res,next){
+
+});
